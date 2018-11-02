b96dafffa24b4f8b8f44e0e1df20c83afd450f76
Fernando Salazar <fslzrr@gmail.com>
2018-11-01 23:12:55 -0600

    DataInit

--- server/lib/dataInit.js
+++ server/lib/dataInit.js
@@ -1,6 +1,23 @@
 const TodoAccess = require("../modules/todo/dataAccess/todo.access");
+const UserAccess = require("../modules/user/dataAccess/user.access");
+const DriverAccess = require("../modules/driver/dataAccess/driver.access");
 
 TodoAccess.create({
   firstLabel: "Hello",
   secondLabel: "World"
+});
+
+UserAccess.createUser({
+  name: "Fernando Salazar",
+  email: "fslzrr@gmail.com",
+  password: "password",
+  phone: "6671321009"
+});
+
+DriverAccess.createDriver({
+  name: "Pedro Picapiedras",
+  email: "picapiedra@roca.com",
+  password: "password",
+  phone: "9991119990",
+  taxiNumber: "666"
 });
--- server/modules/models.js
+++ server/modules/models.js
@@ -1,5 +1,5 @@
 const Todo = require("./todo/models/todo.model");
 const User = require("./user/models/user.model");
-const Driver = require("./user/models/driver.model");
+const Driver = require("./driver/models/driver.model");
 
 // DB Relationships
