0a43736dd64caca2dd2762ce4bb80df94bad1360
Fernando Salazar <fslzrr@gmail.com>
2018-11-04 20:52:44 -0600

    Inside App Routing

---
+++ client/src/pages/Clients/Clients.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import "./Clients.css";
+
+class Clients extends Component {
+  render() {
+    return <div className="Clients">Clients!</div>;
+  }
+}
+
+export default Clients;
---
+++ client/src/pages/Drivers/Drivers.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import "./Drivers.css";
+
+class Drivers extends Component {
+  render() {
+    return <div className="Drivers">Drivers!</div>;
+  }
+}
+
+export default Drivers;
