c358b83064eeba81d26732bc027da46796198376
EduardoHi <eshidalgo97@gmail.com>
2018-11-06 22:39:02 -0600

    Refactor to make exports properly

--- server/modules/driver/dataAccess/driver.access.js
+++ server/modules/driver/dataAccess/driver.access.js
@@ -1,42 +1,50 @@
 const Driver = require("../models/driver.model");
 const { accessErrorHandler } = require("../../../lib/errorHandler");
 
-exports.createDriver = async driver => {
+async function createDriver(driver) {
   try {
     return await Driver.create(driver);
   } catch (err) {
     throw accessErrorHandler(err);
   }
-};
+}
 
-exports.updateDriver = async driver => {
+async function updateDriver(driver) {
   try {
     return await Driver.update(driver, { where: { id: driver.id } });
   } catch (err) {
     throw accessErrorHandler(err);
   }
-};
+}
 
-exports.getAllDrivers = async () => {
+async function getAllDrivers() {
   try {
     return await Driver.findAll();
   } catch (err) {
     throw accessErrorHandler(err);
   }
-};
+}
 
-exports.getById = async id => {
+async function getById(id) {
   try {
     return await Driver.findOne({ where: { id } });
   } catch (err) {
     throw accessErrorHandler(err);
   }
-};
+}
 
-exports.deleteDriver = async id => {
+async function deleteDriver(id) {
   try {
     return await Driver.destroy({ where: { id } });
   } catch (err) {
     throw accessErrorHandler(err);
   }
+}
+
+export default {
+  createDriver,
+  updateDriver,
+  getAllDrivers,
+  getById,
+  deleteDriver
 };
